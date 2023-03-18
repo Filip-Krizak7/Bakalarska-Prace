539db4a1b4169bb44205f7dbaca158682adefb70
Filip-Krizak <r20382@student.osu.cz>
2023-03-18 20:08:25 +0100

    fix: getShoolByID method

--- backend/src/service/SchoolService.cs
+++ backend/src/service/SchoolService.cs
@@ -9,10 +9,10 @@ namespace TeacherPractise.Service
         {
             using (var ctx = new Context())
             {
-                if (!(ctx.Schools.ToList().Exists(q => q.Id = (int)id)))
+                if (!(ctx.Schools.ToList().Exists(q => q.Id == (int)id)))
                 throw AppUserService.CreateException($"School with ID: {id} does not exists.", null);
 
-                School sch = ctx.Schools.ToList().Find(q => (q.Id = (int)id));
+                School sch = ctx.Schools.ToList().Find(q => q.Id == (int)id);
 
                 return sch; 
             }
